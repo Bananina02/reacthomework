0d284b8ab973516b9641ccbcea8558c19a3a27e2
Banachos <gosha.romanovich.2016@mail.ru>
2024-10-29 23:17:17 +0300

    first commit

---
+++ src/Components/ShopItemFunc/ShopItemFunc.jsx
@@ -0,0 +1,19 @@
+import classes from './ShopItemFunc.module.css'
+export const ShopItemFunc = (propt) => {
+    const {brand, title, description, descriptionFull, price, currency} = propt.item
+    return <div className="main-content">
+        <h2 className={classes ['brand']}>{brand}</h2>
+        <h1 className={classes ['title']}>{title}</h1>
+        <h3 className={classes ['description']}>{description}</h3>
+        <div className={classes ['descriptionFull']}>{descriptionFull}</div>
+        <div className={classes ['highlight-window mobile']}>
+        <div className={classes ['highlight-overlay']}></div>
+        </div>
+        <div className={classes ['divider']}>{currency}</div>
+        <div className={classes ['purchase-info']}>
+            <div className={classes ['price']}>{price}</div>
+            <button>Добавить в корзину</button>
+        </div>
+    </div>
+
+}
